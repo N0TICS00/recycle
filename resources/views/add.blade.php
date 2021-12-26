d734bba43192cc7bf3d1f0e218783d45aed4a000
N0TICS0 <74853755+N0TICS0@users.noreply.github.com>
2021-12-26 11:03:43 +0100

    Add Handling controller, add page

---
+++ resources/views/add.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aggiungi un ritiro</title>
+</head>
+<body>
+    <h1>Aggiungi un ritiro</h1>
+    <form action="/" method="POST">
+        <p>
+        <input type="text" name="material" placeholder="Materiale" required>
+        </p>
+        <p>
+        <input type="text" name="day" placeholder="Giorno" required>
+        </p>
+        <p>
+        <input type="text" name="hour" placeholder="Orario" required> 
+        </p>
+        <p>
+        <button type="submit">Aggiungi</button>
+        </p>
+
+    </form>
+</body>
+</html>
