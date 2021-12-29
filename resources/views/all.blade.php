70f0f33d312afd7999fe85cd853fed112a63c383
N0TICS0 <74853755+N0TICS0@users.noreply.github.com>
2021-12-29 11:11:28 +0100

    Added style to all page, fixed bug

--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -7,54 +7,102 @@
     <title>Tutti i ritiri</title>
     <!--BOOTSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!--CSS-->
+    <link rel="stylesheet" href="css/app.css">
 
 </head>
-<body style="text-align:center">
+<style>
+    *{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+}
+body{
+    text-align:center;
+}
+.card-div{
+   width:18rem;
+   margin:50px auto;
+
+}
+
+</style>
+<body >
     <h1>Ritiri in settimana</h1>
-@foreach($retires as $retire)
-        <li>{{$retire->material}} alle ore {{$retire->hour}}, il {{$retire->day}}</li>
-        @endforeach
+        <div class="card-div">
         <div class="card" style="width: 18rem;">
          <div class="card-body">
              <h5 class="card-title">Lunedì</h5>
-             @foreach($retires as $retire){
-                 if($retires->day == 1){
-                    <li>$retire</li>;
-                 }
-             }
-             @endforeach
+        @foreach($retires as $retire)
+            @if($retire->day == "Lunedì")
+                <li>{{$retire->material}} alle ore {{$retire->hour}}</li>
+            @endif
+        @endforeach
+
+
             </div>
         </div>
         <div class="card" style="width: 18rem;">
          <div class="card-body">
              <h5 class="card-title">Martedì</h5>
+            @foreach($retires as $retire)
+                @if($retire->day == "Martedì")
+                    <li>{{$retire->material}} alle ore {{$retire->hour}}</li>
+                @endif
+            @endforeach
             </div>
         </div>
         <div class="card" style="width: 18rem;">
          <div class="card-body">
              <h5 class="card-title">Mercoledì</h5>
+             @foreach($retires as $retire)
+                @if($retire->day == "Mercoledì")
+                    <li>{{$retire->material}} alle ore {{$retire->hour}}</li>
+                @endif
+            @endforeach
             </div>
         </div>
         <div class="card" style="width: 18rem;">
          <div class="card-body">
              <h5 class="card-title">Giovedì</h5>
+             @foreach($retires as $retire)
+                @if($retire->day == "Giovedì")
+                    <li>{{$retire->material}} alle ore {{$retire->hour}}</li>
+                @endif
+            @endforeach
             </div>
         </div>
         <div class="card" style="width: 18rem;">
          <div class="card-body">
              <h5 class="card-title">Venerdì</h5>
+             @foreach($retires as $retire)
+                @if($retire->day == "Venerdì")
+                    <li>{{$retire->material}} alle ore {{$retire->hour}}</li>
+                @endif
+            @endforeach
             </div>
         </div>
         <div class="card" style="width: 18rem;">
          <div class="card-body">
              <h5 class="card-title">Sabato</h5>
+             @foreach($retires as $retire)
+                @if($retire->day == "Sabato")
+                    <li>{{$retire->material}} alle ore {{$retire->hour}}</li>
+                @endif
+            @endforeach
             </div>
         </div>
         <div class="card" style="width: 18rem;">
          <div class="card-body">
              <h5 class="card-title">Domenica</h5>
+             @foreach($retires as $retire)
+                @if($retire->day == "Domenica")
+                    <li>{{$retire->material}} alle ore {{$retire->hour}}</li>
+                @endif
+            @endforeach
             </div>
+        </div>
         </div>
 </body>
 </html>
