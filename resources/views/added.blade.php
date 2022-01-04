7e73dfe2b1702f5c731acc7d17d3edbac25de734
N0TICS0 <74853755+N0TICS0@users.noreply.github.com>
2022-01-04 17:12:28 +0100

    Final Commit

--- resources/views/added.blade.php
+++ resources/views/added.blade.php
@@ -9,7 +9,24 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
-<body style="text-align:center;">
+<style>
+    body{
+        text-align:center
+    }
+    .btn{
+        width:200px;
+    }
+    
+</style>
+<body>
     <h1>Ritiro aggiunto con successo!</h1>
+    <p>
+    <a href="/"><button class="btn btn-md btn-dark">Home</button></a>
+    </p>
+    <a href="/all"><button class="btn btn-md btn-dark">Guarda tutti i ritiri</button></a>
+    </p>
+    <p>
+    <a href="/add"><button class="btn btn-md btn-dark">Aggiungi i ritiri</button></a>
+    </p>
 </body>
 </html>
